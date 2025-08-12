805b1139ac5a789ecbb9afafd2da9877dd8b20ed
fahad-shahjee <cyberfahad021@gmail.com>
2025-08-12 23:58:00 +0500

    sections done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import HomeOffer from "@/components/containers/home/HomeOffer";
 import HomeTestimonial from "@/components/containers/home/HomeTestimonial";
 import HomeBlog from "@/components/containers/home/HomeBlog";
 import HomeSponsor from "@/components/containers/home/HomeSponsor";
+import Portfolio from "@/components/containers/home/Portfolio"
 import NextPage from "@/components/containers/home/NextPage";
 
 const Home = () => {
@@ -16,7 +17,7 @@ const Home = () => {
       <Agency />
       <HomeOffer />
       <HomeTestimonial />
-      <PortfolioText />
+      <Portfolio />
       <HomeBlog />
       <HomeSponsor />
       {/* <NextPage /> */}
