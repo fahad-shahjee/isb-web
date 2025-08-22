25a63d7f459ca452969ab3e8235bbe17686cb66a
fahad-shahjee <cyberfahad021@gmail.com>
2025-08-22 20:54:12 +0500

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import Layout from "@/components/layout/Layout";
 import HomeFourBanner from "@/components/layout/banner/HomeFourBanner";
 import Agency from "@/components/containers/home/Agency";
-import PortfolioText from "@/components/containers/home/PortfolioText";
 import HomeOffer from "@/components/containers/home/HomeOffer";
 import HomeTestimonial from "@/components/containers/home/HomeTestimonial";
-import HomeBlog from "@/components/containers/home/HomeBlog";
 import HomeSponsor from "@/components/containers/home/HomeSponsor";
 import Portfolio from "@/components/containers/home/Portfolio"
 import Cta from "@/components/containers/home/Cta"
-import NextPage from "@/components/containers/home/NextPage";
 
 const Home = () => {
   return (
