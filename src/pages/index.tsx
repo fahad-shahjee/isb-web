a59056b50266caf279beae4f30d0b861c996ad9a
fahad-shahjee <cyberfahad021@gmail.com>
2025-08-27 20:05:25 +0500

    pricing section added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import HomeTestimonial from "@/components/containers/home/HomeTestimonial";
 import HomeSponsor from "@/components/containers/home/HomeSponsor";
 import Portfolio from "@/components/containers/home/Portfolio"
 import Cta from "@/components/containers/home/Cta"
+import PricingSection from "@/components/containers/PricingSection";
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
       <HomeTestimonial />
       <Portfolio />
       <HomeSponsor />
+      <PricingSection />
       {/* <NextPage /> */}
     </Layout>
   );
